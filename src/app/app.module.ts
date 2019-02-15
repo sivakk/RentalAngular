b5fa783cec590af27270cdea1a51537d7233f9e9
sivakk <ksiva1355@gmail.com>
2019-02-15 16:17:33 +0530

    style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import {Routes,RouterModule} from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { TempComponent } from './temp/temp.component';
+import { RentalListComponent } from './rental/rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component';
 
+const routes:Routes=[{path:'hello',component:RentalComponent}];
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RentalComponent,
+    TempComponent,
+    RentalListComponent,
+    RentalListItemComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule
   ],
